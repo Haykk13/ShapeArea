99bfc0f5260b65c59fbf3296c8002cc27b6f864a
Hayk <hayk.hayr3@gmail.com>
2020-08-21 23:11:52 +0400

    Add project files.

---
+++ ShapeArea/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer to know its shape area number: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The shape area number of {n} is {shapeArea(n)}!");
+            Console.ReadLine();
+        }
+        static int shapeArea(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return shapeArea(n - 1) + (2 * n) + 2 * (n - 2);
+            }
+        }
+
+    }
+}
